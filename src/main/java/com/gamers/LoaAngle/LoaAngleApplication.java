a5b1c607b5c819c1a097c972cbec4959eb76b8c7
sw2park <pswoo990803@gmail.com>
2024-10-18 16:20:20 +0900

    24.10.18 first commit

---
+++ src/main/java/com/gamers/LoaAngle/LoaAngleApplication.java
@@ -0,0 +1,13 @@
+package com.gamers.LoaAngle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoaAngleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoaAngleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamers/LoaAngle/LoaAngleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamers.LoaAngle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoaAngleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
